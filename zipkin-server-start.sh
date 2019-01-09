7655c28434dc5c83a7c931839435970c5a8d4bc7
方俊锋 <fangjunfeng@qianmi.com>
2019-01-09 17:19:42 +0800

    Adjusts copyright headers for this year

--- zipkin-server-start.sh
+++ zipkin-server-start.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
+#
+# Copyright 2015-2019 The OpenZipkin Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied. See the License for the specific language governing permissions and limitations under
+# the License.
+#
+
 
 QUERY_LOOKBACK=360000 STORAGE_TYPE=elasticsearch ES_HOSTS=http://172.30.2.6:9200 ES_TIMEOUT=30000 ES_MAX_REQUESTS=128 ES_INDEX_SHARDS=20 java -Xms2g -Xmx2g -jar ./zipkin-server/target/zipkin-server-2.11.13-SNAPSHOT-exec.jar
